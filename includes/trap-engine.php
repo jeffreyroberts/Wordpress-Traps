367ae32044e97d723c9b8454bda1ae51998c94b2
Jeffrey Roberts <jlroberts@Jeffreys-iMac.local>
2015-11-19 14:55:16 -0500

    Trap Engine, For Trap Accounting, Auditing, and Monitoring

--- includes/trap-engine.php
+++ includes/trap-engine.php
@@ -5,3 +5,16 @@
  * Date: 11/19/15
  * Time: 1:56 PM
  */
+
+class TrapEngine {
+
+    function __construct() {
+
+    }
+
+    function run() {
+
+        WPTrap_Data::$trapData['uniqid'] = uniqid('trap');
+
+    }
+}
