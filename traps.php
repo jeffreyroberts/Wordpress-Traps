d3b1761965c9aa37b52e5723853dd3a63410374d
Jeffrey Roberts <Jeffrey.L.Roberts@gmail.com>
2015-11-19 15:55:16 -0500

    v0.1 traps.php

--- traps.php
+++ traps.php
@@ -6,6 +6,10 @@
  * Time: 1:33 PM
  */
 
+include_once '/opt/webapplications/RawPHP/wordpress/wp-config.php';
+include_once '/opt/webapplications/RawPHP/wordpress/wp-includes/post.php';
+include_once '/opt/webapplications/RawPHP/wordpress/wp-includes/meta.php';
+
 class WPTraps_Trap {
 
     function __construct() {
@@ -19,6 +23,15 @@ class WPTraps_Trap {
             // If JSTrap ->
             //      Decrement JS Miss Counter
 
-        } 
+            global $wpdb;
+            $post_ID = '1';
+            $jstraps = get_post_meta($post_ID, '_jstraps', true) != '' ? get_post_meta($post_ID, '_jstraps', true) : '0';
+            $jstrapsNew = $jstraps - 1;
+            update_post_meta($post_ID, '_jstraps', $jstrapsNew);
+
+        }
     }
 }
+
+$traps = new WPTraps_Trap();
+$traps->run();
